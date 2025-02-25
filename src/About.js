ee4a938749a584a4c1cb92c8d37b1dd6e55037b5
sillyU-jpg <nikolaiking7@gmail.com>
2025-02-25 17:09:44 +1300

    cleaned up aesthetic

--- src/About.js
+++ src/About.js
@@ -1,14 +1,18 @@
 const About = () => {
     return (
         <div className="about-section">
+
+          <h1 className="page-title"> About </h1>
+ 
+
         <img src="./project/selfie.png" className="selfie-img" alt="portrait"/>
         <div id="about-text">
-          <h1 className="about-title"> About Me </h1>
-          <h2 className="about-desc"> Web-Developer, Game Developer, and Software designer,
+         
+          <h1 id="about-desc"> Web-Developer, Game Developer, and Software designer,
             Learning new technology every day, check out some of my work located on my projects page.
-          </h2>
+          </h1>
           <div className="contact-section">
-             <h2>Work/Enquiries: </h2> 
+             <h1>Work/Enquiries: </h1> 
              <h3>email: nikolaiking7@gmail.com</h3> 
             <div className="links-section">
              <a href="https://www.linkedin.com/in/nikolai-king-4896a0321/"> <h2>Linkdin</h2></a>
