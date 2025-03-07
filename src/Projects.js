dae4beb73f47049a838f4535667453521fc4a66e
sillyU-jpg <nikolaiking7@gmail.com>
2025-03-07 20:48:15 +1300

    added modal page

--- src/Projects.js
+++ src/Projects.js
@@ -1,27 +1,64 @@
+import React, { useState } from 'react';
 import { ProjectData } from "./projectdata";
 
 const Projects = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const openModal = (project) => {
+    setSelectedProject(project);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedProject(null);
+  };
+
   return (
-    
-    <div className="container">
-      <div className="projects-section">
-        <div id="title-text">
-          <h1 className="page-title"> Personal Projects </h1>
-        </div>
-        <div className="projects-list">
-          {ProjectData.map((ProjectData) => (
-            <div className="project-item" key={ProjectData.title}>
-              <img className="project-image" src={ProjectData.image} alt="" />
-              <a href={ProjectData.link}>  
-                <h2 className="project-title">{ ProjectData.title }</h2> 
-              </a>
-              <p className="project-desc"> { ProjectData.description }</p>
+    <>
+      <div className="container">
+        <div className="projects-section">
+          <div id="title-text">
+            <h1 className="page-title"> Personal Projects </h1>
+          </div>
+          <div className="projects-list">
+            {ProjectData.map((project) => (
+              <div key={project.title}>
+                <div className="project-item">
+                  <img className="project-image" src={project.image} alt="" onClick={() => openModal(project)}/>
+                  <a className="btn-open" onClick={() => openModal(project)}>
+                    <h2 className="project-title">{project.title}</h2>
+                  </a>
+                  <p className="project-desc">{project.description}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+          {isModalOpen && [selectedProject] && (
+            <div className="modal">
+              <div className="modal-header">
+                <h1>{selectedProject.title}</h1>
+                <a className="btn-close" onClick={closeModal}>
+                  &times;
+                </a>
+                </div>
+                <div className="modal-content">
+                  <div className="modal-carousel">
+                <img src={selectedProject.altimg} className='modal-img' />
+                </div>
+                <div className="modal-text"> <h1>{selectedProject.title}</h1>
+                <p>{selectedProject.long}</p>
+                <a href={selectedProject.link}>check out this project</a>
+                </div>
+              </div>
             </div>
-          ))}
+          )}
+          {isModalOpen && <div className="overlay" onClick={closeModal}></div>}
         </div>
       </div>
-    </div>
+    </>
   );
-}
+};
 
 export default Projects;
--- src/projectdata.js
+++ src/projectdata.js
@@ -4,7 +4,9 @@ export const ProjectData = [
     {
       title: "Image-Daily",
       description: "My daily image blog, showcasing a new image from my camera roll each day.",
-      image: './project/imgdaily.JPG',
+      long: "A daily image blog created with vanilla JavaScript, using Firestore as the backend to store images. It also includes an archive page to display all previous entries.",
+      image: './project/imgdaily.png',
+      // altimg: './project/imagedaily/imgdaily1.png',
       link: "https://nikolaiking.com/imagedaily/"
     },
     // {
@@ -17,15 +19,15 @@ export const ProjectData = [
     {
       title: "EcoVision Smart sorting bin",
       description: "An AI powered smart sorting bin, part of my year two works",
-      image: "ecovision.image",
+      image: "./project/ecovision.png",
       link: "/ecovisionbin",
       long: "EcoVision was my second-year Creative Technologies project, integrating motorized robotics for automated waste sorting and bin waste-level detection. At its core was a Python-powered AI system utilizing the YOLOv4 object detection model, trained specifically on New Zealand waste and recycling materials. Additionally, we developed a visual interface that displayed real-time recycling statistics and featured interactive games for educational engagement. The entire system was optimized to run on a Raspberry Pi single-board computer."
     },
     {
       title: "The Robertson gift",
       description: "a 3D interactive replication of the robertson gift, an exhibition from Auckland art gallery.",
-      image: "art gallery 3d recreation image",
-      link: "/robertsongift",
+      image: "/project/robertson.png",
+      link: "robertson",
       long: "For my second-year Synthetic Realities project, I designed and developed a 1:1 digital recreation of The Robertson Gift, an Auckland art gallery exhibition showcasing works from some of the world's most renowned artists. Using Unity, I recreated the exhibit with over 40 featured pieces, incorporating interactive elements that trigger obscure music and soundscapes inspired by the artwork."
     },
     {
